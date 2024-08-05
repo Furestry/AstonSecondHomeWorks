2b7a604abd939cf86fd49707b06a0e71a98e9614
sevler <sevler.u17@gmail.com>
2024-08-05 11:29:30 +0300

    Added check if id is null Removed unused constructor Department(String name)

--- src/main/java/ru/furestry/astonhomework/entity/Department.java
+++ src/main/java/ru/furestry/astonhomework/entity/Department.java
@@ -16,12 +16,41 @@ public class Department implements IEntity {
 
     private List<User> users;
 
-    public Department(String name) {
-        this.name = name;
-    }
-
     public Department(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public int hashCode(){
+        int result = 17;
+
+        if (id != null) {
+            result = 31 * result + id.hashCode();
+        }
+
+        if (name != null) {
+            result = 31 * result + name.hashCode();
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof Department department)) return false;
+        if (id == null) {
+            if (department.getId() == null) {
+                return department.getName().equals(name);
+            }
+
+            return false;
+        }
+        if (department.getId() == null) {
+            return false;
+        }
+
+        return department.getId().equals(id) && department.getName().equals(name);
     }
 }
